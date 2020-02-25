4cbb879079855bd91b6a8f3ca2433355442561fe
Megager <Dimonzalis@gmail.com>
2020-02-25 12:09:16 +0200

    Revert "Revert "Revert "Update selenium.js"""
    This reverts commit d19b41676e600333da19dadad1365d8dad15cdd7.

--- tests/selenium.js
+++ tests/selenium.js
@@ -1,10 +1,5 @@
-web.transaction('Initialization');
 web.init()
-
-web.transaction('Homepage');
 web.open('https://automationbookstore.dev/')
-
-web.transaction('Search');
 web.type('id=searchBar', 'Agile Testing')
 web.waitForExist('li.ui-screen-hidden')
 var visibleBooks = web.findElements('li:not(.ui-screen-hidden)').length
