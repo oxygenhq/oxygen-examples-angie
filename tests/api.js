c34af1f3284ab4c47f6f7d0cc6279ba358489b94
romovs <romovs@gmail.com>
2021-03-23 12:07:54 +0200

    Update api and email tests

--- tests/api.js
+++ tests/api.js
@@ -1,6 +1,6 @@
 var response = http.get(
     'https://www.googleapis.com/books/v1/volumes?q=9781451648546', 
     {'content-type':'application/json'})
-log.info(JSON.stringify(response))
-assert.equal(response.totalItems, 1709, "Total items incorrect")
-assert.equal(response.items[0].id, 'nvgdAwAAQBAJ', "Item ID incorrect")
+log.info(response)
+assert.equal(response.body.totalItems, 1709, "Total items incorrect")
+assert.equal(response.body.items[0].id, 'nvgdAwAAQBAJ', "Item ID incorrect")
