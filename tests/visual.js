9589266d4967cbd5c337463088db2a2fe66f2da8
romovs <romovs@gmail.com>
2021-03-23 12:10:01 +0200

    Update visual test

--- tests/visual.js
+++ tests/visual.js
@@ -1,5 +1,6 @@
-// add WinAppDriver script
 web.init()
+eyes.init('web');
+web.open('https://automationbookstore.dev/')
 web.type('id=searchBar', 'Agile Testing')
 web.waitForExist('li.ui-screen-hidden')
-eyes.checkWindow('Agile Testing Books 1')
+eyes.checkWindow('Oxygen: Agile Testing book')
