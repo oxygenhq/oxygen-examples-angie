8eceefed6c002690c7486a76c1fc07be5f745b5b
romovs <romovs@gmail.com>
2023-03-28 13:45:31 +0300

    Update env

--- oxygen.env.js
+++ oxygen.env.js
@@ -2,11 +2,19 @@ module.exports = {
     default: {
         baseUrl: "https://automationbookstore.dev/",
         boolTestA: true,
-        boolTestB: false
+        boolTestB: false,
+        intTest: 4,
+        intStrTest: 5,
+        floatTest: 454.454,
+        complexTest: { a: 4, b: true, c: false, d: "blalala"}
     },
     prod: {
         baseUrl: "http://prod.automationbookstore.dev",
         boolTestA: true,
-        boolTestB: false
+        boolTestB: false,
+        intTest: 4,
+        intStrTest: 5,
+        floatTest: 454.454,
+        complexTest: { a: 4, b: true, c: false, d: "blalala"}
     }
 };
