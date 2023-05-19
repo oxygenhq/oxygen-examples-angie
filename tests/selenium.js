4c6a2e2e0c4554c5fe005ca8349f1e2764e46514
romovs <romovs@gmail.com>
2023-05-19 13:15:07 +0300

    .

--- tests/selenium.js
+++ tests/selenium.js
@@ -5,7 +5,9 @@ web.transaction('Homepage');
 web.open('https://automationbookstore.dev/')
 
 web.transaction('Search'); 
-web.type('id=searchBar', 'Agile Testing')
+web.type('id=searchBar', env.contentFilePath)
+web.setTimeout(1000)
+web.assertTitle('sds')
 web.waitForExist('li.ui-screen-hidden')
 var visibleBooks = web.findElements('li:not(.ui-screen-hidden)').length
 assert.equal(visibleBooks, 1, "There should only be one book visible")
