814136b3d3c99a0cff67bf591331fda5012ff612
romovs <romovs@gmail.com>
2024-05-29 16:32:49 +0300

    Remove prod

--- oxygen.env.js
+++ oxygen.env.js
@@ -10,17 +10,7 @@ module.exports = {
         floatTest: 454.454,
         regex: /.*/
     },
-    prod: {
-        baseUrl: "http://prod.automationbookstore.dev",
-        boolTestA: true,
-        boolTestB: false,
-        intTest: 4,
-        intStrTest: "5",
-        contentFilePath:"\\\\menora\\DATA\\USERS\\PUBLIC\\Cloudbeat_QA\\hafaka\\QA\\",                 
-        writeToFileFilepath: "D:\\Project\\Tashlumim\\",
-        floatTest: 454.454,
-        regex: /.*/
-    },
+
     new: {
         baseUrl: "http://prod.automationbookstore.dev",
         boolTestA: true,
