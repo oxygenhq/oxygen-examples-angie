c1cac384397d977ee03284d65c404191c0d74418
romovs <romovs@gmail.com>
2025-01-31 17:12:41 +0200

    Add env

--- oxygen.env.js
+++ oxygen.env.js
@@ -21,5 +21,18 @@ module.exports = {
         writeToFileFilepath: "D:\\Project\\Tashlumim\\",
         floatTest: 454.454,
         regex: /.*/
+    },
+
+
+    new2: {
+        baseUrl: "http://prod.automationbookstore.dev",
+        boolTestA: true,
+        boolTestB: false,
+        intTest: 4,
+        intStrTest: "5",
+        contentFilePath:"\\\\menora\\DATA\\USERS\\PUBLIC\\Cloudbeat_QA\\hafaka\\QA\\",                 
+        writeToFileFilepath: "D:\\Project\\Tashlumim\\",
+        floatTest: 454.454,
+        regex: /.*/
     }
 };
