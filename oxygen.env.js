869c33451aa649b1f3e2309811cb83403487fcbd
romovs <romovs@gmail.com>
2025-08-07 12:23:45 +0300

    .

--- oxygen.env.js
+++ oxygen.env.js
@@ -9,7 +9,7 @@ module.exports = {
         writeToFileFilepath: "D:\\Project\\Tashlumim\\",
         floatTest: 454.454,
         regex: /.*/,
-        test: "foo\Kar",
+        test: "foo\bar",
         test2: "foo2\\bar2"
         test3: "foo3/bar3",
         test4: "foo4//bar4"
